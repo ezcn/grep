ec63bf1950940a742da257b92afcf5494a9f3bbc
Enza Colonna <enza.colonna@gmail.com>
2019-05-20 17:38:15 +0200

    Update stats.R

--- databse/stats.R
+++ databse/stats.R
@@ -18,14 +18,15 @@ myd1$pregnancy_termination_date <- as.Date(myd1$pregnancy_termination_date, "%m/
 myd1$last_menstruation_date  <- as.Date(myd1$last_menstruation_date, "%m/%d/%Y")
 myd1$birth_date  <- as.Date(myd1$birth_date, "%m/%d/%Y")
 
-
+# 4. put induced NA as 0 
+myd1$miscarriage[is.na(myd1$miscarriage)] <- 0
 ######### GESTATIONAL AGE 
 
 ### remove  id!="AS064" & id!="AV148" & id!="AV082"
 #### check using : myd1 %>% filter(id=="AV082") %>% select (pregnancy_termination_date, last_menstruation_date) 
 
 mysGesAge=subset(myd1, id!="AS064" & id!="AV148" & id!="AV082")
-mysGesAge$miscarriage[is.na(mysGesAge$miscarriage)] <- 0
+#mysGesAge$miscarriage[is.na(mysGesAge$miscarriage)] <- 0
 mysGesAge$GesAgeWeeks =difftime(mysGesAge$pregnancy_termination_date, mysGesAge$last_menstruation_date, units = "weeks")
 
 ggplot(mysGesAge, aes(type_of_miscarriage, GesAgeWeeks ) )+
@@ -38,11 +39,8 @@ theme_bw() + ggtitle("Gestational age \nat pregnancy termination")
 ######### AGE OF THE MOTHER
 
 mysMotAge=subset(myd1, !is.na(myd1$pregnancy_termination_date) | !is.na(myd1$birth_date) )
-mysMotAge$miscarriage[is.na(mysMotAge$miscarriage)] <- 0
 mysMotAge$MotherAge<- difftime(mysMotAge$pregnancy_termination_date, mysMotAge$birth_date, units="days")/365  
  
-
-
 ggplot(mysMotAge, aes(type_of_miscarriage, MotherAge ) )+
 geom_boxplot(outlier.shape=NA )+geom_jitter( aes(color=as.factor(miscarriage))) +
 theme_bw() + ggtitle("Age of mother \nat pregnancy termination")
@@ -60,9 +58,23 @@ pvalRPLFPL=formatC(wilcox.test(as.numeric(FPL),as.numeric(RPL), alternative="les
 ggplot(mysMotAge, aes(type_of_miscarriage, MotherAge) )+ geom_boxplot()+geom_jitter(aes(color=as.factor(miscarriage) ) ) + annotate("text", x = 2.5 , y = 20, label = paste ("Mann-Whitney p-value\n(FPL, RPL)  =  ",pvalRPLFPL , sep="")) +theme_bw() + ggtitle("Mother age \nat pregnancy termination")
 #dev.off()
 
-
-
 mysMotAge  %>% group_by(type_of_miscarriage) %>% summarize(medAgeMother=median(MotherAge, na.rm=T)) 
-
 #avAgeMother=mean(MotherAge, na.rm=T)) , minAgeMother=min(MotherAge, na.rm=T) , maxAgeMother=max(MotherAge,  na.rm=T), sdAgeMother=sd(MotherAge, na.rm=T)  )
+
+
+
+######### MENARCHE AGE 
+VTPmen=subset(myd1, type_of_miscarriage=="induced")$menarche_age
+FPLmen=subset(myd1, type_of_miscarriage=="miscarriage_first")$menarche_age
+RPLmen=subset(myd1, type_of_miscarriage=="miscarriage_recurrent")$menarche_age
+
+pvalVTPFPLmen=formatC(var.test(as.numeric(VTPmen),as.numeric(FPLmen), alternative="two.sided")$p.value,  digits = 3) 
+pvalVTPRPLmen=formatC(var.test(as.numeric(VTPmen),as.numeric(RPLmen), alternative="two.sided")$p.value,  digits = 3) 
+pvalFPLPRPLmen=formatC(var.test(as.numeric(FPLmen),as.numeric(RPLmen), alternative="two.sided")$p.value, digits = 3) 
+
+
+ggplot(myd1, aes(type_of_miscarriage, menarche_age) )+ geom_boxplot(outlier.shape=NA) + geom_jitter(aes(color=as.factor(miscarriage)) ) +theme_bw() + ggtitle("Menarche Age") + annotate("text", x = 1.5, y = 8, label = paste ("F-test p-value\n(VTP, FPL) = ",pvalVTPFPLmen , sep=""))+ annotate("text", x = 1.5, y = 9, label = paste ("F-test p-value\n (VTP, RPL) = ",pvalVTPRPLmen , sep=""))  + annotate("text", x = 2.5, y = 9, label = paste ("F-test p-value\n (FPL, RPL) = ",pvalFPLPRPLmen , sep=""))
+
+
+
 
