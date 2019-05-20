6990269764bebbd9c089c2473fdb9a9b694fb13a
Enza Colonna <enza.colonna@gmail.com>
2019-05-20 17:23:16 +0200

    Update stats.R

--- databse/stats.R
+++ databse/stats.R
@@ -37,4 +37,32 @@ theme_bw() + ggtitle("Gestational age \nat pregnancy termination")
 
 ######### AGE OF THE MOTHER
 
-oo
+mysMotAge=subset(myd1, !is.na(myd1$pregnancy_termination_date) | !is.na(myd1$birth_date) )
+mysMotAge$miscarriage[is.na(mysMotAge$miscarriage)] <- 0
+mysMotAge$MotherAge<- difftime(mysMotAge$pregnancy_termination_date, mysMotAge$birth_date, units="days")/365  
+ 
+
+
+ggplot(mysMotAge, aes(type_of_miscarriage, MotherAge ) )+
+geom_boxplot(outlier.shape=NA )+geom_jitter( aes(color=as.factor(miscarriage))) +
+theme_bw() + ggtitle("Age of mother \nat pregnancy termination")
+
+VTP=subset(mysMotAge, type_of_miscarriage=="induced")$MotherAge
+FPL=subset(mysMotAge, type_of_miscarriage=="miscarriage_first")$MotherAge
+RPL=subset(mysMotAge, type_of_miscarriage=="miscarriage_recurrent")$MotherAge
+
+pvalFPLVTP=formatC(wilcox.test(as.numeric(VTP),as.numeric(FPL), alternative="less")$p.value, format = "e", digits = 3) 
+pvalRPLVTP=formatC(wilcox.test(as.numeric(VTP),as.numeric(RPL), alternative="less")$p.value, format = "e", digits = 3) 
+pvalRPLFPL=formatC(wilcox.test(as.numeric(FPL),as.numeric(RPL), alternative="less")$p.value, format = "e", digits = 3) 
+
+
+#png("ageateventinducedfiltered.png", res=300, units="cm", width=20, height =15)
+ggplot(mysMotAge, aes(type_of_miscarriage, MotherAge) )+ geom_boxplot()+geom_jitter(aes(color=as.factor(miscarriage) ) ) + annotate("text", x = 2.5 , y = 20, label = paste ("Mann-Whitney p-value\n(FPL, RPL)  =  ",pvalRPLFPL , sep="")) +theme_bw() + ggtitle("Mother age \nat pregnancy termination")
+#dev.off()
+
+
+
+mysMotAge  %>% group_by(type_of_miscarriage) %>% summarize(medAgeMother=median(MotherAge, na.rm=T)) 
+
+#avAgeMother=mean(MotherAge, na.rm=T)) , minAgeMother=min(MotherAge, na.rm=T) , maxAgeMother=max(MotherAge,  na.rm=T), sdAgeMother=sd(MotherAge, na.rm=T)  )
+
