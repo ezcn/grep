2da156b4d24be810f690e82deeaebd0e0e06e9d2
Enza Colonna <enza.colonna@gmail.com>
2019-05-27 15:00:53 +0200

    Update aneuploidies.R

--- qfpcr/aneuploidies.R
+++ qfpcr/aneuploidies.R
@@ -1,2 +1,2 @@
 myd=read.table("qfarray_summary.tsv_version1", header=T , sep="\t" ,  na ="na" )
-ggplot(subset(myd, !is.na(qf_outcome )  |  qf_type!="contaminata") , aes(qf_type) ) +geom_bar(stat="count")+ coord_flip() +theme_bw()
+ggplot(subset(myd, !is.na(qf_outcome )  |  qf_type!="contaminata") , aes(qf_type) ) +geom_bar(stat="count")+ coord_flip() +theme_bw() +ggtitle("Aneuploidies at chr XXXXXXXX ")
