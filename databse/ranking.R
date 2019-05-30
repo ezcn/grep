31d9d3b4807aa2715fde82b08768bd81854dbfbb
Enza Colonna <enza.colonna@gmail.com>
2019-05-30 15:23:39 +0200

    Update ranking.R

--- databse/ranking.R
+++ databse/ranking.R
@@ -1,5 +1,20 @@
+library(ggpubr)
+library(ggplot2) 
+library(dplyr) 
+#library(gridExtra)
+#library (grid) 
+#library(lattice) 
+
+
 myd=read.table("samples.score", header=T , sep="\t" , na ="na" )
 myd=read.table("/home/enza/oogaProtocol/IMMA/1_samplechoice_medicalrecords/samples.score" , header=T ,sep="\t", na="na" )
+
+# 3. make dates as dates 
+myd$Pregnancy_termination_date <- as.Date(myd$Pregnancy_termination_date, "%m/%d/%Y")
+myd$Last_menstruation_date  <- as.Date(myd$Last_menstruation_date, "%m/%d/%Y")
+myd$v_Nascita  <- as.Date(myd$v_Nascita, "%m/%d/%Y")
+
+
 
 mys=subset(myd, Type=="Miscarriages")
 
@@ -12,6 +27,9 @@ mys$category=ifelse (mys$nor_score< cutoff_nor_score,  "Not-Prioritized",  "Prio
 countYES <- length(which(mys$category == "Prioritized" )) 
 countNO <- length(which(mys$category == "Not-Prioritized" )) 
 
+
+mycol=c("#00AFBB", "#E7B800") 
+
 pAge<-ggplot(mys, aes((Pregnancy_termination_date - v_Nascita)/365 , fill=category) )+ geom_density(alpha=0.4) +theme_bw() + ggtitle("Mother age at pregnancy termination")+ xlab("Years ")+ ylab("count") + scale_fill_manual(values=mycol) 
 
 
@@ -21,19 +39,13 @@ pMen<-ggplot(mys, aes(v_Menarche_age , fill=category) )+ geom_density(alpha=0.4)
 pMis<-ggplot(mys, aes(v_Miscarriage , fill=category) )+ geom_density(alpha=0.4) +theme_bw() + ggtitle("Number of Previous miscarriages")+ xlab("Cases ")+ ylab("count") + scale_fill_manual(values=mycol) 
 
 
-#~~~~~~~~~~~~~~~~ make panel 
+#~~~~~~~~~~~~~~~~ make panel_1 
+#lay <- rbind(c(1,2,3))             
+#myplot<- grid.arrange(pAge, pMen, pMis, nrow = 1, layout_matrix = lay)
+#ggsave("panelPrioritiz.png", plot = myplot, dpi=300, units="cm", width=35, height =10)
 
-library(gridExtra)
-library (grid) 
-library(lattice) 
-
-lay <- rbind(c(1,2,3)) 
-             
-myplot<- grid.arrange(pAge, pMen, pMis, nrow = 1, layout_matrix = lay)
-ggsave("panelPrioritiz.png", plot = myplot, dpi=300, units="cm", width=35, height =10)
-
-### SILVIA anche per gli altri plot: provare a fare uan sola legenda  per tutti e tre i plots 
-
+#~~~~~~~~~~~~~~~~ make panel_2        
+ggarrange(pAge,pMen, pMis, nrow=1, ncol=3, labels = c("A", "B", "C"), common.legend = TRUE, legend = "right")
 
 
 #~~~~~~~~~~~~~~~ tests 
