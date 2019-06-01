19433607c609ae53e0cf0dfe473195d112459ba2
SissiB <49904266+SissiB@users.noreply.github.com>
2019-06-01 11:43:18 +0200

    Update dnastats.R

--- dnaextraction/dnastats.R
+++ dnaextraction/dnastats.R
@@ -1,5 +1,5 @@
 myd=read.table("/home/enza/oogaProtocol/IMMA/abortion_db/fromValentina/RowDataMiscarriageDNAnames.tsv", header=T , na =NA, sep="\t"  )
-
+str
 myd$method <-(ifelse(myd$Extraction=="QIAmp®", "Membrane", ifelse(myd$Extraction=="InstaGene™", "Chelex resin",  "Nucleon resin" )))
 myd$method<- factor(myd$method, levels=c("Chelex resin", "Nucleon resin", "Membrane"))
 
@@ -10,8 +10,7 @@ mycol=c("#00AFBB", "#E7B800", "#FC4E07")
 
 pDNA<- ggplot(myd, aes(type, (ng.ul*Vol.ul)/1000,  color=Extraction ) ) +geom_boxplot() +facet_grid(Type ~ . , scales="free_y") +scale_color_manual(values=mycol ) +scale_fill_manual (values=mycol ) +theme_bw()  +ylab ("Total Amount of DNA per PoC (ug)" )  +ggtitle("DNA extraction from PoC" )
 ggsave("DNAextraction.png", plot= pDNA, device="png", width = 20, height = 15, units = "cm", dpi = 300)
-
-#silvia metter micro invece di u - cambiare induced ...VTP PL - nome legenda "DNA extraction kit"  
+ 
 
 > myd %>% group_by(Type  )  %>% tally() 
 # A tibble: 3 x 2
