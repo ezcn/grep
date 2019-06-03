694becce891d9ddbcd115defa3c855f2170f79a0
Enza Colonna <enza.colonna@gmail.com>
2019-06-03 15:34:43 +0200

    Update arraycgh.R

--- arraycgh/arraycgh.R
+++ arraycgh/arraycgh.R
@@ -19,7 +19,42 @@ imma.win <- winsorize(imma.spread, pos.unit = "bp", arms = NULL, method = "mad",
 ### to save files 
 #imma.segments <- pcf(data=imma.win, gamma=10, assembly="hg19", return.est=TRUE, save.res=TRUE , file.names=c("imma.win.pcf", "imma.win.segments"))
 ### to continue protocol ######## DO NOT USE NORMALIZATION!!!! 
-imma.gamma=10 
+imma.gamma=10 imma.gamma=10 
+#imma.gamma=5
+
+imma.segments <- pcf(data=imma.win, gamma=imma.gamma , assembly="hg19", return.est=TRUE, save.res=FALSE,  normalize = FALSE )
+
+summary(imma.segments$segments$mean) 
+sd(imma.segments$segments$mean) 
+
+## 3. CALLING 
+imma.thr.gain= mean(imma.segments$segments$mean)+3*sd(imma.segments$segments$mean)
+imma.thr.loss= mean(imma.segments$segments$mean)-3*sd(imma.segments$segments$mean)
+plotAberration(segments=imma.segments, thres.gain=imma.thr.gain , thres.loss =imma.thr.loss)
+
+imma.cnvCalls <- callAberrations(imma.segments$segments, imma.thr.gain, imma.thr.loss) 
+summary((imma.cnvCalls$end.pos-imma.cnvCalls$start.pos)/1000000)
+min((imma.cnvCalls$end.pos-imma.cnvCalls$start.pos))
+min((imma.cnvCalls$n.probes))
+
+imma.cnvCalls.gainloss <- imma.segments$segments %>% filter (mean<imma.thr.gain |  mean > imma.thr.gain )
+imma.cnvCalls.gainloss$type="PLS"
+
+## 4. COMPARISON WITH AGILENT 
+## format agilent reference calls and add to seg compare 
+mc=read.table("../array2/all.cyto.tsv.forcomparison", header=T , sep="\t" )
+seg.agilent= cbind.data.frame(sampleID=mc$sampleid, chrom=mc$Chr,  arm=as.character(mc$Chr) ,  start.pos=mc$Start,  end.pos=mc$Stop_bp,  n.probes=as.numeric(mc$Probes), mean=mc$Amp.Gain.Loss.Del) 
+seg.agilent$type="Agilent"
+
+##  rbind 
+all.cnvCalls=rbind(imma.cnvCalls.gainloss, seg.agilent) 
+ggplot(all.cnvCalls, aes((end.pos-start.pos)/1000000, n.probes, color=type ) )+geom_point(alpha=0.6 ) +theme_bw() +facet_grid(type ~ . )
+
+
+
+
+#imma.gamma=5
+
 imma.segments <- pcf(data=imma.win, gamma=imma.gamma , assembly="hg19", return.est=TRUE, save.res=FALSE,  normalize = FALSE )
 
 summary(imma.segments$segments$mean) 
