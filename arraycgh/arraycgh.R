3cd8b4b23cbaf3fa134fb6932515c3bde3ea2365
Enza Colonna <enza.colonna@gmail.com>
2019-06-03 10:59:41 +0200

    Update arraycgh.R

--- arraycgh/arraycgh.R
+++ arraycgh/arraycgh.R
@@ -3,22 +3,23 @@ imma=read.table("/home/enza/oogaProtocol/IMMA/2_arraycgh/array2/all.arraychr.hea
 #### remove duplicates  (artifact from this particular experiment)
 imma.noduplicat <- imma %>% distinct(chr, start, as_sample , .keep_all = TRUE) 
 
-toexclude <- c("AS015_bad", "AS030_bad", "AS036_bad",  "AS065_bad", "AS078_bad" ,  "AS080_bad" , "AS093_bad", "AS032_3xchr22" ) 
-
+toexclude <- c("AS015_bad", "AS030_bad", "AS036_bad",  "AS065_bad", "AS078_bad" ,  "AS080_bad" , "AS093_bad") # , "AS032_3xchr22" ) 
+# toexclude <- c("cicci" )
 #### spread
 imma.spread<- imma.noduplicat %>% filter( !(as_sample  %in% toexclude)  )   %>%  spread(as_sample , LogRatio )
 
-
-
+## 1. WINSORIZATION 
 ###to save in files 
 #imma.win <- winsorize(imma.spread, pos.unit = "bp", arms = NULL, method = "mad", tau = 2.5,k = 25, gamma = 40, iter = 1, assembly = "hg19", digits = 4,return.outliers = FALSE, save.res = TRUE, file.names =c("all.arraychr.head.tsv.forCopynumber.winsorized", "all.arraychr.head.tsv.forCopynumber.outliers") ,verbose = TRUE) 
 ### to continue protocol 
 imma.win <- winsorize(imma.spread, pos.unit = "bp", arms = NULL, method = "mad", tau = 2.5,k = 25, gamma = 40, iter = 1, assembly = "hg19", digits = 4,return.outliers = FALSE, save.res = FALSE  ,verbose = TRUE) 
 
+## 2. SEGMENTATION 
+######## DO NOT USE NORMALIZATION!!!! 
 ### to save files 
 #imma.segments <- pcf(data=imma.win, gamma=10, assembly="hg19", return.est=TRUE, save.res=TRUE , file.names=c("imma.win.pcf", "imma.win.segments"))
-### to continue protocol 
-imma.segments <- pcf(data=imma.win, gamma=10, assembly="hg19", return.est=TRUE, save.res=FALSE )
+### to continue protocol ######## DO NOT USE NORMALIZATION!!!! 
+imma.segments <- pcf(data=imma.win, gamma=10, assembly="hg19", return.est=TRUE, save.res=FALSE,  normalize = FALSE )
 
 summary(imma.segments$segments$mean) 
      Min.   1st Qu.    Median      Mean   3rd Qu.      Max. 
@@ -26,6 +27,11 @@ summary(imma.segments$segments$mean)
 sd(imma.segments$segments$mean) 
 [1] 0.1153323
 
-imma.thr.gain= mean(imma.segments$segments$mean)+4*sd(imma.segments$segments$mean)
-imma.thr.loss= mean(imma.segments$segments$mean)-4*sd(imma.segments$segments$mean)
+
+## 3. CALLING 
+imma.thr.gain= mean(imma.segments$segments$mean)+3*sd(imma.segments$segments$mean)
+imma.thr.loss= mean(imma.segments$segments$mean)-3*sd(imma.segments$segments$mean)
 plotAberration(segments=imma.segments, thres.gain=imma.thr.gain , thres.loss =imma.thr.loss)
+
+imma.cnvCalls <- callAberrations(imma.segments$segments, imma.thr.gain, imma.thr.loss) 
+
