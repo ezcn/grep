ff78740874fc2e22a19449777bc040fb3d4c0a67
GianlucaDamaggio <47500877+GianlucaDamaggio@users.noreply.github.com>
2019-06-18 12:08:43 +0200

    Create kore-markdup.sh

---
+++ sequenceanalysis/kore-markdup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+## flr=file_row
+idflrbam=${idrbam}
+
+# set some parameters for qsub
+
+### -N name of the job
+###$ -N al$idsample
+
+# -q name of the queue to use
+#$ -q bld.q
+
+# number of threads in multi-threaded jobs 
+#$ -pe smp 16
+
+# -M emailaddress@organization.xx, where to send email alerts
+#$ -M gianluca.damaggio@igb.cnr.it
+
+# condition occurred under which email is to be sent, es=end,suspend
+#$ -m es
+
+##~/bin/sambamba sort ${idflrbam}.raw.bam
+~/bin/sambamba markdup ${idflrbam}.raw.sorted.bam /mpbastudies3/IMMA/samples/${idflrbam}.bam
