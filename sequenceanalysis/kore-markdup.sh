618b7da5bb95e2d4beb8c8e4989891d235d37488
gdama <47500877+GianlucaDamaggio@users.noreply.github.com>
2019-06-19 21:23:50 +0200

    Update kore-markdup.sh

--- sequenceanalysis/kore-markdup.sh
+++ sequenceanalysis/kore-markdup.sh
@@ -9,10 +9,10 @@ idflrbam=${idrbam}
 #$ -q bld.q
 
 # -l mf=amount of memory requested (this is a MANDATORY parameter), use carefully.
-#$ -l mf=2G
+#$ -l mf=4G
 
 # number of threads in multi-threaded jobs 
-#$ -pe smp 16
+#$ -pe smp 8
 
 # -M emailaddress@organization.xx, where to send email alerts
 #$ -M gianluca.damaggio@igb.cnr.it
@@ -21,4 +21,4 @@ idflrbam=${idrbam}
 #$ -m es
 
 ##~/bin/sambamba sort ${idflrbam}.raw.bam
-~/bin/sambamba markdup -t 16 -p --tmpdir /scratch /mpbastudies3/IMMA/samples/${idflrbam}.raw.sorted.bam /mpbastudies3/IMMA/samples/${idflrbam}.bam
+~/bin/sambamba markdup -t 8 -p --tmpdir /scratch --overflow-list-size 500000 /mpbastudies3/IMMA/samples/${idflrbam}.raw.sorted.bam /mpbastudies3/IMMA/samples/${idflrbam}.bam
