ab8d3be6ecca5a8a5dbedceb7287d9141d07fadc
SissiB <49904266+SissiB@users.noreply.github.com>
2019-06-27 12:34:31 +0200

    Update BAMstats.R

--- sequenceanalysis/statsRscript/BAMstats.R
+++ sequenceanalysis/statsRscript/BAMstats.R
@@ -11,10 +11,20 @@ kable(sn, caption="Summary numbers")
 #Insert sizes
 is <- grep("^IS",data, value=TRUE)
 is <- separate(data.frame(is),col=1, into=c("ID", "insert size","all pairs", "inward", "outward", "other"), sep="\t")[,-1]
+ism <- melt(is,is="size")
+
+g<-ggplot(data = is, aes(as.numeric(get("insert size")))) + geom_line(aes(y=as.numeric(get("inward"))),color="blue") +  geom_line(aes(y=as.numeric(get("outward"))),color="red") + geom_line(aes(y=as.numeric(get("other"))),color="green") + labs( x = "insert size", y = "all pairs", title ="Mapped insert sizes", subtitle = "All Pairs", caption = "all pairs insert size")
+ggsave("insert_size.png", plot= g, device="png", width = 20, height = 15, units = "cm", dpi = 300)
+
 
 #ACGT content per cycle
 actg <- grep("^GCC",data, value=TRUE)
 actg <- separate(data.frame(actg),col=1, into=c("ID", "cycle", "A", "C", "G", "T", "N", "O"), sep="\t")[,-1]
+actgm <- melt(actg,id="cycle")
+
+ic <- ggplot(actgm, aes(as.numeric(cycle), as.numeric(value), by=variable, colour=variable)) + geom_boxplot() + labs(x= "cycle", y= "count", title= "Base composition(AS074)")
+ggsave("base_comp.png", plot= ic, device="png", width = 20, height = 15, units = "cm", dpi = 300)
+
 
 #First Fragment Qualitites
 q <- grep("^FFQ|^LFQ",data, value=TRUE)
@@ -24,9 +34,19 @@ fq <- separate(data.frame(fq),col=1, into=c("Pair", "Cycle", seq(43)), sep="\t")
 gc <- grep("^GCF|^GCL",data, value=TRUE)
 gc <- separate(data.frame(gc),col=1, into=c("Pair", "GC", "Count"), sep="\t")
 
+h<-ggplot(gc, aes(as.numeric(GC), as.numeric(Count)/sum(as.numeric(Count)),color=Pair)) + geom_line()  + labs( x = "GC", y = "percent", title ="GC content(AS074)")
+ggsave("GCcontent.png", plot= h, device="png", width = 20, height = 15, units = "cm", dpi = 300)
+
+
 #Indel distribution
 id <- grep("^ID",data, value=TRUE)
 id <- separate(data.frame(id),col=1, into=c("ID", "length", "insertion_count", "deletion_count"), sep="\t")[,-1]
+id$length <- as.numeric(as.character(id$length))
+idm <- melt(id,id="length")
+
+k<-ggplot(idm, aes(x=length, y=value, color=variable)) + geom_line(size=1.5) + labs (x= "length", y= "indels", title="Indel distribution(AS074)") 
+ggsave("indels.png", plot= k, device="png", width = 20, height = 15, units = "cm", dpi = 300)
+
 
 #Indels per cycle
 ic <- grep("^IC",data, value=TRUE)
@@ -42,5 +62,7 @@ cov$bases<-as.numeric(as.character(cov$bases))
 pcov<-ggplot(cov, aes(x = coverage, y = bases)) + geom_bar(stat = "identity")  + coord_cartesian(xlim=c(0,150)) + labs(title= "Coverage distribution(AS074)")
 ggsave("coverageAS074.png", plot= pcov, device="png", width = 20, height = 15, units = "cm", dpi = 300)
 
-
+#Make panel
+myplot<- grid.arrange(pcov,pcov90,pcov94,pcov06,pcov54,pcov64, nrow = 2)
+ggsave("coverage.png", plot = myplot, dpi=300, units="cm", width=35, height =30)
 
