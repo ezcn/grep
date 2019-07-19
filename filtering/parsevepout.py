b23954f61517ce5896e4f370150eff35290ddb7d
Enza Colonna <enza.colonna@gmail.com>
2019-07-19 11:49:38 +0200

    Update parsevepout.py

--- filtering/parsevepout.py
+++ filtering/parsevepout.py
@@ -1,9 +1,14 @@
-myinputvep=open("testvep.tsv", "r")
-myvepcsq=open ("csqimpact.tsv", "r")
+## Take as input the output of VEP and outputs a table with chr pos allele *score*
+## *score* is based on VEP consequence rank need to be IMproved 
 
-##  READ VEP consequences rank ########
+
+myinputvep=open("testvep.tsv", "r") ## tab delimited out of vepcsq
+myvepcsq=open ("csqimpact.tsv", "r") ## https://www.ensembl.org/info/genome/variation/prediction/predicted_data.html
+
+##  1) READ VEP consequences rank ########
+## read VEP rank and create dSOTermRank: dictionary with score assigend to SOTerm based on dRank  *IMPROVE scoring*  
 dRank={"HIGH":4, "LOW": 2, "MODERATE":3, "MODIFIER":1}
-dSOTermRank={}
+dSOTermRank={} 
 
 countlinesCsq= True
 for csqLine in myvepcsq:
@@ -12,14 +17,14 @@ for csqLine in myvepcsq:
                 countlinesCsq=False
         else:
                 myCsqList=csqLine.rstrip().split('\t')
-                dCsq= dict(zip(csqTitle, myCsqList ))
+                dCsq= dict(zip(csqTitle, myCsqList ))  ## create a temporary dictionary with the title column and the content of the row 
                 #print("##########################" )
                 #print (dCsq)
                 dSOTermRank[dCsq['SO term']]=dRank[dCsq['IMPACT']]
                 #print('~~~~~~~~~~~~~~~~~')
 #print( dSOTermRank)
 
-###   Process VEP output
+## 2) Process VEP output
 countlines=0
 for line in myinputvep:
         if countlines ==0 :
@@ -27,6 +32,6 @@ for line in myinputvep:
                 countlines+=1
         else:
                 myrow=line.rstrip().split()
-                drow= dict(zip(title, myrow ))
+                drow= dict(zip(title, myrow ))   ## create a temporary dictionary with the title column and the content of the row 
                 #print(drow)
                 print ( drow['Chr'], drow['Pos'], drow['Allele'], dSOTermRank[drow['Consequence'] ])
