4b5c1e83700d934ad10b58c26536dd79d5821e54
SissiB <49904266+SissiB@users.noreply.github.com>
2019-09-25 14:08:48 +0200

    Create kore-vtnormalizeFb.sh

---
+++ sequenceanalysis/kore-vtnormalizeFb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+id=${id}
+chr=${chr}
+
+# -q name of the queue to use
+#$ -q bld.q
+
+# -l mf=amount of memory requested (this is a MANDATORY parameter), use carefully.
+#$ -l mf=2G
+
+# number of threads in multi-threaded jobs 
+#$ -pe smp 4 
+
+singularity exec -B /mpbastudies3/IMMA/samples/fb/:/data -B /mpbastudies3/IMMA/hg38p12/:/ref /mpba0/mpba-sw/biocontainers/vt.img vt normalize /data/filtered/${id}.${chr}.fb.filt.vcf.gz -r /ref/hg38.p12.fa -o  /data/normalized/${id}.${chr}.fb.norm.vcf.gz
