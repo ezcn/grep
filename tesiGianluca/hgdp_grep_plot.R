f97e2713bd02fb6acd5882659f0c33afef94b67e
GianlucaDamaggio <gianlucadamaggio@yahoo.it>
2019-10-22 09:49:09 +0200

    upload

--- tesiGianluca/hgdp_grep_plot.R
+++ tesiGianluca/hgdp_grep_plot.R
@@ -16,8 +16,6 @@ grep$CSQfreq <- as.numeric(as.character(grep$CSQfreq))
 
 ### uso tutti i cicli
 hgdpSel<-select(hgdp,Chr,Pos,VariantClass,CSQallele,CSQrank,Consequence,CSQfreq,REFfreq,ALTfreq,MAF,Population)
-#hgdpFiltC1 <- hgdpSel %>% filter ( Cycle == 1 )
-#hgdpC1<-select(hgdpFiltC1,Chr,Pos,VariantClass,CSQallele,CSQrank,Consequence,CSQfreq,REFfreq,ALTfreq,MAF,Population)
 full <- rbind(hgdpSel,grep)
 
 ### scelgo solo 1 ciclo in particolare
@@ -29,35 +27,20 @@ full <- rbind(hgdpSel,grep)
 
 ########
 
-#hgdpMean<- hgdpC1 %>% filter ( Consequence == "upstream_gene_variant")
-#hgdpMean$CSQfreq <- as.numeric(as.character(hgdpMean$CSQfreq))
-#hgdpMean<-hgdpMean %>% summarize(Mean= mean(CSQfreq ))
-#sdHGDP<- hgdpMean %>% sd(CSQfreq)
-#grepMean<- grep %>% filter ( Consequence == "upstream_gene_variant")
-#grepMean$CSQfreq <- as.numeric(as.character(grepMean$CSQfreq))
-#grepMean<-grepMean %>% summarize(Mean=mean(CSQfreq ))
-#zscore<- (grepMean$Mean-hgdpMean$Mean)/sdHGDP
 
-#dens <- density(hgdpMean$CSQfreq)
-#plot(dens, frame = FALSE, col = "steelblue", 
-#     main = "Density plot of CSQfreq")
+myplot<-ggplot(subset(full, !is.na(CSQfreq)) , aes(x=Population, y=CSQfreq, fill= VariantClass )) + geom_boxplot(position = position_dodge(preserve = "single")) + facet_wrap( ~ Consequence)
 
-#myplot<-ggplot(subset(hgdpC1, !is.na(CSQfreq)) , aes(CSQfreq )) + geom_histogram() + geom_point(x=zscore)
-
-########
-
-myplot<-ggplot(subset(full, !is.na(CSQfreq)) , aes(x=Population, y=CSQfreq, fill= VariantClass )) + geom_boxplot() + facet_wrap( ~ Consequence)
 
 ggsave("/mpba0/vcolonna/gianluca/TESI/hgdp/bothPos/plotWrapConsequence.png", plot = myplot, dpi=300, units="cm", width=50, height =40)
 
-myplot2<-ggplot(subset(full, CSQfreq > 0,  !is.na(CSQfreq)) , aes(x=Population, y=CSQfreq, fill= VariantClass )) + geom_boxplot() + facet_wrap( ~ Consequence)
+myplot2<-ggplot(subset(full, CSQfreq > 0,  !is.na(CSQfreq)) , aes(x=Population, y=CSQfreq, fill= VariantClass )) + geom_boxplot(position = position_dodge(preserve = "single")) + facet_wrap( ~ Consequence)
 
 ggsave("/mpba0/vcolonna/gianluca/TESI/hgdp/bothPos/plotWrapConsequenceCSQfreqMTZ.png", plot = myplot2, dpi=300, units="cm", width=50, height =40)
 
-myplot3<-ggplot(subset(full, !is.na(CSQfreq)) , aes(x=Population, y=CSQfreq, fill= VariantClass )) + geom_boxplot() + facet_wrap( ~ CSQrank)
+myplot3<-ggplot(subset(full, !is.na(CSQfreq)) , aes(x=Population, y=CSQfreq, fill= VariantClass )) + geom_boxplot(position = position_dodge(preserve = "single")) + facet_wrap( ~ CSQrank)
 
 ggsave("/mpba0/vcolonna/gianluca/TESI/hgdp/bothPos/plotWrapCSQrank.png", plot = myplot3, dpi=300, units="cm", width=50, height =40)
 
-myplot4<-ggplot(subset(full,CSQfreq>0, !is.na(CSQfreq)) , aes(x=Population, y=CSQfreq, fill= VariantClass )) + geom_boxplot() + facet_wrap( ~ CSQrank)
+myplot4<-ggplot(subset(full,CSQfreq>0, !is.na(CSQfreq)) , aes(x=Population, y=CSQfreq, fill= VariantClass )) + geom_boxplot(position = position_dodge(preserve = "single")) + facet_wrap( ~ CSQrank)
 
 ggsave("/mpba0/vcolonna/gianluca/TESI/hgdp/bothPos/plotWrapCSQrankMTZ.png", plot = myplot4, dpi=300, units="cm", width=50, height =40)
