308d442ccfd921aa5b0d535f6c7167690d75ed5f
Flavia95 <52487106+Flavia95@users.noreply.github.com>
2019-11-25 10:29:29 +0100

    Update plotLDfromPlink.R

--- sequenceanalysis/plotLDfromPlink.R
+++ sequenceanalysis/plotLDfromPlink.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(ggplot2) 
 library(reshape2) 
 library(gplots)
+library(gaston)
 
 #####1.import data 
 path_chr9 <- ('hgdp_wgs.eur.2Mbrs7859844.ld.gz.ld') 
@@ -37,6 +38,16 @@ my_palette <- colorRampPalette(c("white", "yellow","orange", "red"))(n = 299)
 png("LD.png", width = 20, height = 15, units = "cm", res = 300) 
 heatmap.2(myM, key.xlab="LD", cellnote = myMask, notecol="black", Colv = NA, Rowv = NA, scale = "none", col = my_palette, trace = "none", density.info = "none", margins = c(7,14), main = "Linkage Disequilibrium")
 
+dev.off()
+
+#####5. new plot
+data_9_matrix <- dcast(data_9s, BP_A ~ BP_B, value.var = "R2") # convert to matrix with column AND rownames 
+myM <- as.matrix(data_9_matrix[ , -1 ]) 
+row.names(myM) <- data_9_matrix$BP_A #I am converting all NAs to 0, reconsider if this is suitable in your case. 
+myM[ is.na(myM) ] <- 0
+
+png("Ldchr9.png", width = 20, height = 15, units = "cm", res = 300)
+LD.plot(myM)
 dev.off()
 
 #####5.job  [kore-plotLDfromPlink.sh]
