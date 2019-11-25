38fc4f68ff51d185445308d741caeefc350ab1f2
GianlucaDamaggio <gianlucadamaggio@yahoo.it>
2019-11-25 10:08:25 +0100

    upload

--- tesiGianluca/AFS-hgdpMean.py
+++ tesiGianluca/AFS-hgdpMean.py
@@ -72,12 +72,10 @@ def main():
 ##########~~~~~~~~~~~~~~  Loop of vcf lines 
 
 	sys.stdout=open(args.o, 'w') 
-	#print(lSOTerm)
 	listOfErrors=[]
 	print('Chr','\t','Number_of_Cycle','\t','SNV_mean','\t','Indel_mean','\t','SequenceAlt_mean','\t','Insertion_mean','\t','transcript_ablation','\t','splice_acceptor_variant','\t','splice_donor_variant','\t','stop_gained','\t','frameshift_variant','\t','stop_lost','\t','start_lost','\t','transcript_amplification','\t','inframe_insertion','\t','inframe_deletion','\t','missense_variant','\t','protein_altering_variant','\t','splice_region_variant','\t','incomplete_terminal_codon_variant','\t','start_retained_variant','\t','stop_retained_variant','\t','synonymous_variant','\t','coding_sequence_variant','\t','mature_miRNA_variant','\t','5_prime_UTR_variant','\t','3_prime_UTR_variant','\t','non_coding_transcript_exon_variant','\t','intron_variant','\t','NMD_transcript_variant','\t','non_coding_transcript_variant','\t','upstream_gene_variant','\t','downstream_gene_variant','\t','TFBS_ablation','\t','TFBS_amplification','\t','TF_binding_site_variant','\t','regulatory_region_ablation','\t','regulatory_region_amplification','\t','feature_elongation','\t','regulatory_region_variant','\t','feature_truncation','\t','intergenic_variant')
 	cycle=0
 	element=['SNV','indel','sequence_alteration','deletion','insertion']
-	#element=['SNV_c','indel_c','sequence_alteration_c','deletion_c','insertion_c','SNV_f','indel_f','sequence_alteration_f','deletion_f','insertion_f','SNV_m','indel_m','sequence_alteration_m','deletion_m','insertion_m'] # how many observation 
 	counter=[]
 	for i in element:
 		counter.append(0)
@@ -89,7 +87,6 @@ def main():
 		myinput=gzip.open(args.f, 'r')
 		dInfo={}
 		column2retain=[]
-		#ddata=dict(zip(element,counter))
 		dcount=dict(zip(element,counter))
 		dmean=dict(zip(element,counter))
 		dfreq=dict(zip(element,counter))
@@ -149,35 +146,10 @@ def main():
 						if dCsq['Consequence'] in key and freqCSQ_REF_ALT[0]!='NA':
 							dSOTcount[dCsq['Consequence']]+=int(1)
 							dSOTfreq[dCsq['Consequence']]+=float(freqCSQ_REF_ALT[0])
-							#print(dSOTcount,'\t',dSOTfreq)
 					for key in dcount:
 						if dCsq['VARIANT_CLASS'] in key and freqCSQ_REF_ALT[0]!='NA':
 							dcount[dCsq['VARIANT_CLASS']]+=int(1)
 							dfreq[dCsq['VARIANT_CLASS']]+=float(freqCSQ_REF_ALT[0])
-							#print(dcount,'\t',dfreq)
-					#if dCsq['VARIANT_CLASS']=='SNV' and freqCSQ_REF_ALT[0]!='NA':
-					#	ddata['SNV_c']+=int(1)
-					#	ddata['SNV_f']+=float(freqCSQ_REF_ALT[0])
-					#	#print('this is a new SNV')
-					#	#print(ddata['SNV_c'],'\t',ddata['SNV_f'])
-					#if dCsq['VARIANT_CLASS']=='indel' and freqCSQ_REF_ALT[0]!='NA':
-					#	ddata['indel_c']+=int(1)
-					#	ddata['indel_f']+=float(freqCSQ_REF_ALT[0])
-					#if dCsq['VARIANT_CLASS']=='sequence_alteration' and freqCSQ_REF_ALT[0]!='NA':
-					#	ddata['sequence_alteration_c']+=int(1)
-					#	ddata['sequence_alteration_f']+=float(freqCSQ_REF_ALT[0])
-					#if dCsq['VARIANT_CLASS']=='insertion' and freqCSQ_REF_ALT[0]!='NA':
-					#	ddata['insertion_c']+=int(1)
-					#	ddata['insertion_f']+=float(freqCSQ_REF_ALT[0])
-					#
-							
-		#listmean=['SNV_m','indel_m','sequence_alteration_m','insertion_m']
-		#print(dfreq)
-		#print(dcount)
-		#for item in listmean:
-		#	for elem in dfinal:
-		#		if item in dfinal:
-		#			dfinal[item]=(dfinal[item]/dfinal[item])
 		for key in dcount:
 			if dcount[key]!=0:
 				dmean[key]=(dfreq[key]/dcount[key])
@@ -185,27 +157,10 @@ def main():
 			if dSOTcount[key]!=0:
 				dSOTmean[key]=(dSOTfreq[key]/dSOTcount[key])
 		#--------------------------
-		#ddata['SNV_m']=(float(ddata['SNV_f'])/ddata['SNV_c'])
-		#ddata['indel_m']=(float(ddata['indel_f'])/ddata['indel_c'])
-		#ddata['sequence_alteration_m']=(float(ddata['sequence_alteration_f'])/ddata['sequence_alteration_c'])
-		#ddata['insertion_m']=(float(ddata['insertion_f'])/ddata['insertion_c'])
 		
-		#print(dmean)
 		print(mychr,'\t',cycle,'\t',dmean['SNV'],'\t',dmean['indel'],'\t',dmean['sequence_alteration'],'\t',dmean['insertion'],'\t',dSOTmean['transcript_ablation'],'\t',dSOTmean['splice_acceptor_variant'],'\t',dSOTmean['splice_donor_variant'],'\t',dSOTmean['stop_gained'],'\t',dSOTmean['frameshift_variant'],'\t',dSOTmean['stop_lost'],'\t',dSOTmean['start_lost'],'\t',dSOTmean['transcript_amplification'],'\t',dSOTmean['inframe_insertion'],'\t',dSOTmean['inframe_deletion'],'\t',dSOTmean['missense_variant'],'\t',dSOTmean['protein_altering_variant'],'\t',dSOTmean['splice_region_variant'],'\t',dSOTmean['incomplete_terminal_codon_variant'],'\t',dSOTmean['start_retained_variant'],'\t',dSOTmean['stop_retained_variant'],'\t',dSOTmean['synonymous_variant'],'\t',dSOTmean['coding_sequence_variant'],'\t',dSOTmean['mature_miRNA_variant'],'\t',dSOTmean['5_prime_UTR_variant'],'\t',dSOTmean['3_prime_UTR_variant'],'\t',dSOTmean['non_coding_transcript_exon_variant'],'\t',dSOTmean['intron_variant'],'\t',dSOTmean['NMD_transcript_variant'],'\t',dSOTmean['non_coding_transcript_variant'],'\t',dSOTmean['upstream_gene_variant'],'\t',dSOTmean['downstream_gene_variant'],'\t',dSOTmean['TFBS_ablation'],'\t',dSOTmean['TFBS_amplification'],'\t',dSOTmean['TF_binding_site_variant'],'\t',dSOTmean['regulatory_region_ablation'],'\t',dSOTmean['regulatory_region_amplification'],'\t',dSOTmean['feature_elongation'],'\t',dSOTmean['regulatory_region_variant'],'\t',dSOTmean['feature_truncation'],'\t',dSOTmean['intergenic_variant'])
-		#print(cycle,'\t',ddata['SNV_m'],'\t',ddata['indel_m'],'\t',ddata['sequence_alteration_m'],'\t',ddata['insertion_m'])
-							
-	#mean=(float(SNVfreq)/SNVcount)		
-	#print(cycle,'\t',mean)
-							
-	#sys.stdout.close()
-
-						#print(linesplit[0],'\t',linesplit[1],'\t',dCsq['VARIANT_CLASS'],'\t',dCsq['Allele'],'\t',dSOTermRank[mostSevereCsq],'\t',cons,'\t',freqCSQ_REF_ALT[0],'\t',freqCSQ_REF_ALT[1],'\t',freqCSQ_REF_ALT[2],'\t',freqCSQ_REF_ALT[3],'\t',cycle,'\t','HGDP')
 
 
-	#fileToWrite=open(args.e, 'w')
-	#for i in listOfErrors: fileToWrite.write( i )
-	#sys.stdout.close()
 	
-
 if __name__ == '__main__':
 	main()
