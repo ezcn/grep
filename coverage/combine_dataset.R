afe74758871b410745756355e6dbf6c1c0b0fd8b
SissiB <49904266+SissiB@users.noreply.github.com>
2019-11-28 09:35:00 +0100

    Update combine_dataset.R

--- coverage/combine_dataset.R
+++ coverage/combine_dataset.R
@@ -1,4 +1,4 @@
-############### combine ARRAY, PROBES AND RANDOM #############################3
+############### combine ARRAY, PROBES AND RANDOM files#############################3
 
 #1) array outcome file
 
@@ -15,6 +15,8 @@ arr$chrom<-as.character(arr$chrom)
 
 setwd("/home/silvia/misc/coverageCNV/table")
 
+#~ Probes (same position and distribution of array CGH probes)
+
 probe<- data.frame(sampleID=character(), chrom=character(), start.pos=numeric(), end.pos=numeric(), Zcov=numeric(), pval=numeric(), nprobes=numeric(), type=character(), coverage=character(), variables=character(), stringsAsFactors=FALSE)
 
 
@@ -30,6 +32,7 @@ for (ss in c("30X", "15X", "1X")){
 }
 
 
+#~ 360k random probes
 
 random360<- data.frame(sampleID=character(), chrom=character(), start.pos=numeric(), end.pos=numeric(), Zcov=numeric(), pval=numeric(), nprobes=numeric(), type=character(), coverage=character(), variables=character(), stringsAsFactors=FALSE)
 
@@ -45,7 +48,7 @@ for (ss in c("30X", "15X", "1X")){
    
 }
 
-
+#~ 60k random probes
 
 random60<- data.frame(sampleID=character(), chrom=character(), start.pos=numeric(), end.pos=numeric(), Zcov=numeric(), pval=numeric(), nprobes=numeric(), type=character(), coverage=character(), variables=character(), stringsAsFactors=FALSE)
 
@@ -61,14 +64,13 @@ for (ss in c("30X", "15X", "1X")){
    
 }
 
-#total<-rbind(probe,random,arr)
-
-
 
 ################################# Different gamma and kmin ######################################
 
 
 #3) depth files with different segmentation (gamma=2 ;kmin=5)
+
+#~ Probes
 
 probe2<- data.frame(sampleID=character(), chrom=character(), start.pos=numeric(), end.pos=numeric(), Zcov=numeric(), pval=numeric(), nprobes=numeric(), type=character(), coverage=character(), variables=character(), stringsAsFactors=FALSE)
 
@@ -84,7 +86,7 @@ for (ss in c("30X", "15X", "1X")){
    
 }
 
-
+#~ 360k random probes
 
 random360_2<- data.frame(sampleID=character(), chrom=character(), start.pos=numeric(), end.pos=numeric(), Zcov=numeric(), pval=numeric(), nprobes=numeric(), type=character(), coverage=character(), variables=character(), stringsAsFactors=FALSE)
 
@@ -99,6 +101,8 @@ for (ss in c("30X", "15X", "1X")){
     random360_2=rbind(random360_2, mydf)
    
 }
+
+#~ 60k random probes
 
 random60_2<- data.frame(sampleID=character(), chrom=character(), start.pos=numeric(), end.pos=numeric(), Zcov=numeric(), pval=numeric(), nprobes=numeric(), type=character(), coverage=character(), variables=character(), stringsAsFactors=FALSE)
 
@@ -115,10 +119,11 @@ for (ss in c("30X", "15X", "1X")){
 }
 
 #~~~~~~~~~~~~~~~~~~~~merge all files ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
 total<-rbind.data.frame(arr,probe,random60,random360,probe2,random60_2,random360_2)
 
-total$coverage <- factor(total$coverage, levels = c("30X", "15X", "1X", "array"))
-total$type <- factor(total$type, levels=c("array", "probes", "random60k", "random360k"))
+total$coverage <- factor(total$coverage, levels = c("30X", "15X", "1X", "array")) #new total$coverage order
+total$type <- factor(total$type, levels=c("array", "probes", "random60k", "random360k")) #new total$type order
 
 
 
@@ -129,18 +134,16 @@ ggplot(total, aes(end.pos-start.pos, fill=type, linetype=variables ) ) +geom_den
 
 write.table(total, "/home/silvia/misc/coverageCNV/table/complete_CoverageVsArray.tsv", quote=F, sep="\t", row.names=F, col.names=T)
 
-#ggplot(total2, aes(end.pos-start.pos, fill=type ) ) +geom_density(aes(alpha=0.4 ) )+facet_grid (coverage ~ . )+ scale_x_log10( )
+
 
 #~~~~~~~~~~~~~~~~~~~~~~ compare
 
 ggplot(subset(total, chrom==12 ) , aes(x=start.pos, y=Zcov, xend=end.pos, yend=Zcov) )+ geom_segment(aes(color=type  )) + ylim (-1, 2.5 )+facet_wrap(sampleID ~ . )
 
+# new variable to plot only values >0.95 quantile e <0.05 
 
+new<-myd %>% filter(sampleID=="AS054" | sampleID=="AS064" |sampleID=="AS074" |sampleID=="AS094" |sampleID=="AS090" ) %>% group_by(type,newvar) %>% mutate(plot=(ifelse(type=="array", "yes", ifelse(Zcov>quantile(Zcov, .95) | Zcov<quantile(Zcov, .05), "yes", "no"))))
 
-new<-total %>% group_by(type,variables) %>% mutate(plot=(ifelse(type=="array", "yes", ifelse(Zcov>quantile(Zcov, ,95) |  (Zcov<quantile(Zcov, ,5), "yes", "no"))))
+new$plotf=as.factor(new$plot)
 
-
-#new<-total %>% group_by(type,variables) %>% mutate(plot=(ifelse(type=="array", "yes", ifelse(Zcov>quantile(Zcov, 95 & 5), "yes", "no")))) %>% select(sampleID, chrom, start.pos, end.pos, Zcov, pval, nprobes,type, coverage, variables, plot)
-
-
-
+ggplot(subset(new, plotf=="yes" & chrom==8 & sampleID=="AS064" ) , aes(x=start.pos, y=Zcov, xend=end.pos, yend=Zcov) )+ geom_segment(aes(color=type  )) +  facet_wrap(sampleID ~ . ) + geom_hline(yintercept=0, color="grey" )+theme_minimal()
