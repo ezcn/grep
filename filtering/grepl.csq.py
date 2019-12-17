bf4c25fa97a20d9754c28cd5281066af8193fb61
ezcn <enza.colonna@gmail.com>
2019-12-17 15:14:45 +0100

    weighted average

--- filtering/grepl.csq.py
+++ filtering/grepl.csq.py
@@ -56,7 +56,7 @@ def main():
 	filemyres=open(args.o, 'w')
 	listOfErrors=[]
 	dInfo={}
-	header=["chr", "pos", "Existing_variation", "quality", "csqAllel", "csqAlleleCount", "GTLiklihood" , "ENSTID", "ImpactScore", "FineImpactScore", "rare","Embryo","GnomAD","CellCycle","DDD","miscarriage","lethal", "essential", "gpscore"]
+	header=["chr", "pos", "Existing_variation", "quality", "csqAllel", "csqAlleleCount", "GTLiklihood" , "ENSTID", "ImpactScore", "FineImpactScore", "rare","Embryo","GnomAD","CellCycle","DDD","miscarriage","lethal", "essential", "gpscore", "wAvGpScore"]
 	filemyres.write("\t".join(map(str, header)))
 	filemyres.write("\n")   
 	
@@ -97,7 +97,7 @@ def main():
 				##~~ single consequence
 				#print ('~~~  this is a consequence in a line ')
 				for mcsq in multipleCsq:    
-					myres=[]; gpScore=0
+					myres=[]; gpScore=0; gpweig=0
 					myres+=[mychr, mypos]
 					dCsq=dict(zip(csqHeader, mcsq.split("|") ))  #############    ALL VEP INFO 
 					#print (dCsq) 
@@ -116,7 +116,7 @@ def main():
 						myres+=featMultiOut; 
 						csqAllCount=int(featMultiOut[1])
 						gpScore+=featMultiOut[1] * dWeig['wCAC']
-
+						gpweig+=dWeig['wCAC']	
 					#~~~~~~~~~~~ append ENSTID to myres	
 					if  dCsq['Feature'] is not '':	myres.append(dCsq['Feature'])
 					else: myres.append('na')
@@ -128,7 +128,8 @@ def main():
 					mostSevereCsq=lSOTerm[min(myindexes)]
 					myres.append( dSOTermRank[mostSevereCsq ]) ## score based on the impact of the consequence       	
 					myres.append( dSOTermFineRank[mostSevereCsq ])
-					gpScore+=dSOTermFineRank[mostSevereCsq ]* dWeig['wRank']	
+					gpScore+=dSOTermFineRank[mostSevereCsq ]* dWeig['wRank']
+					gpweig+=dWeig['wRank']	
 					#~~~~~~~~~~~~~~ find out if a variant is rare 
 					listOfRelevantPopulations=[]	# vector with 
 					listOfVEPPopulations=["AFR_AF", "AMR_AF", "EAS_AF", "EUR_AF", "SAS_AF", "gnomAD_AFR_AF", "gnomAD_AMR_AF", "gnomAD_ASJ_AF", "gnomAD_EAS_AF", "gnomAD_FIN_AF", "gnomAD_NFE_AF", "gnomAD_OTH_AF", "gnomAD_SAS_AF"] #data from VEP data format is a float or NULL (empty )
@@ -143,39 +144,39 @@ def main():
 				
 					rare = gp.checkFreq (freqlist, args.r) # check if it is a rare variant (af<thresh)  
 					myres.append(rare)
-					if rare== True: gpScore+=1*dWeig['wRare'] 
-					elif rare=="NOB": gpScore+=1*dWeig['wNOB']
-				
+					if rare== True: gpScore+=1*dWeig['wRare'] ; gpweig+=dWeig['wRare']
+					elif rare=="NOB": gpScore+=1*dWeig['wNOB']; gpweig+=dWeig['wNOB']
+					
 					#~~~~~~~~~~ check if row have Embryo,CellCycle,DDD,GmomAD genes
 					embryo = DDD = cellcycle = gnomAD = miscarriages = lethal = essential = False  
 				
 					###~~~  Gene Ontology  embryo development GO:0009790	
-					if re.search("ANN_1", decodedLine): embryo=True; gpScore+=1*dWeig['wEmbryoDev']
+					if re.search("ANN_1", decodedLine): embryo=True; gpScore+=1*dWeig['wEmbryoDev']; gpweig+=dWeig['wEmbryoDev']
 					myres.append(embryo)
 
 					###~~~ gene in Loss of Function list 	source? 
-					if re.search("ANN_4", decodedLine): gnomAD=True; gpScore+=1*dWeig['wGnomeAD']
+					if re.search("ANN_4", decodedLine): gnomAD=True; gpScore+=1*dWeig['wGnomeAD']; gpweig+=dWeig['wGnomeAD']
 					myres.append(gnomAD)
 
 					###~~~ gene ontology cell cycle  GO:XXXXX
-					if re.search("ANN_3", decodedLine): cellcycle=True; gpScore+=1*dWeig['wCellCycle']
+					if re.search("ANN_3", decodedLine): cellcycle=True; gpScore+=1*dWeig['wCellCycle']; gpweig+=dWeig['wCellCycle']
 					myres.append(cellcycle)
 
 					###~~~ DDD
-					if re.search("ANN_2", decodedLine): DDD=True; gpScore+=1*dWeig['wDDD']
+					if re.search("ANN_2", decodedLine): DDD=True; gpScore+=1*dWeig['wDDD'];  gpweig+=dWeig['wDDD']
 					myres.append(DDD)
 					
 					###~~~ List of miscarriages Madhuri 
-					if re.search("ANN_5", decodedLine): miscarriages=True; gpScore+=1*dWeig['wMisc']
+					if re.search("ANN_5", decodedLine): miscarriages=True; gpScore+=1*dWeig['wMisc']; gpweig+=dWeig['wMisc']
 					myres.append(miscarriages)
 
 					###~~~ List of candidate lethal genes
-					if re.search("ANN_6", decodedLine): lethal=True; gpScore+=1*dWeig['wLethal']
+					if re.search("ANN_6", decodedLine): lethal=True; gpScore+=1*dWeig['wLethal']; gpweig+=dWeig['wLethal']
 					myres.append(lethal)
 
                                         ###~~~ List of essential genes
 					
-					if re.search("essential", decodedLine): essential=True; gpScore+=(1*dWeig['wEssential'])*int(dInfo['essential'])
+					if re.search("essential", decodedLine): essential=True; gpScore+=dWeig['wEssential']*int(dInfo['essential']); gpweig+=dWeig['wEssential']
 					myres.append(essential)
 
 					##~~~~~~~~~~ ANNOVAR 
@@ -190,7 +191,7 @@ def main():
 				
 					#if dSOTermFineRank[mostSevereCsq ] > args.i and rare==True and embryo==True or dSOTermFineRank[mostSevereCsq ] > args.i and rare==True and cellcycle==True: #or dSOTermFineRank[mostSevereCsq ] > args.i and rare==True and DDD==True or dSOTermFineRank[mostSevereCsq ] > args.i and rare==True and gnomAD==True:	
 					#if dSOTermFineRank[mostSevereCsq ] > args.i and csqAllCount>args.c: #and rare==True and csqAllCount>args.c: 
-					myres.append(gpScore)
+					myres.append(gpScore, gpScore/float(gpweig) )
 					filemyres.write("\t".join( map(str, myres ) ) )
 					filemyres.write('\n')
 
