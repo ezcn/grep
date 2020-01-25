b4d143790398fcc2610e4218e9ae16ef65e10341
ezcn <enza.colonna@gmail.com>
2020-01-25 07:22:07 +0100

    annotations on grep.csq.local.py

--- filtering/testparsevepjson.py
+++ filtering/testparsevepjson.py
@@ -15,65 +15,61 @@ def checkFreq (listFreq, threshold):
 	return(rare)
 
 vepInfo={}
-with open(filename, 'r') as f:
+#~~ filename is the json output of VEP runned locally 
+#~~ filename is parsed by line and by alternate allele  
+with open(filename, 'r') as f:  
 	for line in f:
-		#print('#####')
-		info=json.loads(line)
+		info=json.loads(line) #~~ make a dictionary out of a string 
 		#print(line_d['input']) 
-		locusdata=info['input'].split(); altAlleles=locusdata[4].split(",")
+		#~~ derive the key chr:pos:/alt from the "input" element and process each alternate allele 
+		locusdata=info['input'].split(); altAlleles=locusdata[4].split(","); mychr=locusdata[0]; mypos=locusdata[1]
 		for altAl in altAlleles:
-			mykey=locusdata[0].lstrip("chr") + ":" + locusdata[1] + ":/" + altAl
+			mykey=mychr.lstrip("chr") + ":" + mypos + ":/" + altAl
 			vepInfo[mykey]={}	
 
+			#~~ check if the most severe consequence is in the line else retrun an empty vepInfo[mykey] 
 			if "most_severe_consequence" in info:
 				vepInfo[mykey]["most_severe_consequence"]=info["most_severe_consequence"]
 				most=vepInfo[mykey]["most_severe_consequence"]
+				#~~  check if the most sequence is in a transcript else in a regulatory feature 
 				if 'transcript_consequences' in info:
 					ReportedGeneId=[] ; ReportedGeneSymbol=[]
-					for i in info['transcript_consequences']:
-						if most in  i['consequence_terms'] :
-							csqAllele=i['variant_allele']
+					for tc in info['transcript_consequences']:
+						if most in  tc['consequence_terms'] :
+							csqAllele=tc['variant_allele']
 							if csqAllele ==altAl : 
-								print (most, csqAllele, i["transcript_id"], i['gene_id'])
+								print (most, csqAllele, tc["transcript_id"], tc['gene_id'])
 								vepInfo[mykey]['csqAllele']=csqAllele
-								if not i['gene_id'] in ReportedGeneId: ReportedGeneId.append(i['gene_id'])
-								if not i['gene_symbol'] in ReportedGeneSymbol:  ReportedGeneSymbol.append(i['gene_symbol']) 
+								if not tc['gene_id'] in ReportedGeneId: ReportedGeneId.append(tc['gene_id'])
+								if not tc['gene_symbol'] in ReportedGeneSymbol:  ReportedGeneSymbol.append(tc['gene_symbol']) 
 					vepInfo[mykey]['gene_id']=ReportedGeneId
 					vepInfo[mykey]['gene_symbol']=ReportedGeneSymbol
 
 				elif 'regulatory_feature_consequences' in info: 
-					for r  in info['regulatory_feature_consequences']:
-						if most in  r['consequence_terms']:
-							csqAllele=r['variant_allele']
+					for rf  in info['regulatory_feature_consequences']:
+						if most in  rf['consequence_terms']:
+							csqAllele=rf['variant_allele']
 							if csqAllele ==altAl :
 								vepInfo[mykey]['csqAllele']=csqAllel
+
+			#~~ retrive info on rsID, starting position, frequencies
 			if "colocated_variants" in info:
 				id_search = info["colocated_variants"][0]
 				if "id" in id_search:
 			    		vepInfo[mykey]["id"] = id_search["id"]
-				# adding info for starting position, this is needed for merging with CADD score.
-				#if "start" in id_search: 
-				#	vepInfo[mykey]["starting_position"] = id_search["start"]   
+				adding info for starting position, this is needed for merging with CADD score.
+				if "start" in id_search: 
+					vepInfo[mykey]["starting_position"] = id_search["start"]   
+				#~~ check if variant is rare according to threshold given as argument and report True or False in vepInfo[mykey]
 				if 'frequencies' in id_search:
 					frqInfoDic=id_search["frequencies" ]
 					for i in frqInfoDic: 
 						if vepInfo[mykey]['csqAllele'] in frqInfoDic: 
-							
-							#to_parse = list(id_search["frequencies"].values())[0]
-							to_parse = list(frqInfoDic[vepInfo[mykey]['csqAllele']].values())#[0]
+							to_parse = list(frqInfoDic[vepInfo[mykey]['csqAllele']].values())
 							#print(to_parse)
 							vepInfo[mykey]['rare']= checkFreq (to_parse, 0.01) 		
 							
  	
-					#for var in to_parse.keys():
-					#	vepInfo[mykey][var]=to_parse[var]
 
-
-
-			
-
-#print (json.dumps(info, indent=4 ) )  
+print (json.dumps(info, indent=4 ) )  
 print (json.dumps(vepInfo, indent=4) ) 
-#datastore = json.dumps(filecontent)
-#for i in mydata: 
-#	print (i ) 
