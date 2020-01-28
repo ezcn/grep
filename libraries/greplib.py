63a91b1feeff5cc66248aa133c5d599f3e296801
Silvia Buonaiuto <silvia@ui03.recas.ba.infn.it>
2020-01-28 16:36:16 +0100

    new greplib.py

--- libraries/greplib.py
+++ libraries/greplib.py
@@ -1,6 +1,90 @@
 #!/usr/bin/python3
-import requests, json 
+#import requests, json 
+import json 
 
+def getInfoFromVepLocally (jsonWithVEPannotations, thresholdRareAllele ):  
+	vepInfo={}
+	#~~ filename is the json output of VEP runned locally 
+	#~~ filename is parsed by line and by alternate allele  
+	with open(jsonWithVEPannotations, 'r') as f:  
+		for line in f:
+			info=json.loads(line) #~~ make a dictionary out of a string 
+			#print(line_d['input']) 
+			#~~ derive the key chr:pos:/alt from the "input" element and process each alternate allele 
+			locusdata=info['input'].split(); altAlleles=locusdata[4].split(","); mychr=locusdata[0]; mypos=locusdata[1]
+			for altAl in altAlleles:
+				mykey=mychr.lstrip("chr") + ":" + mypos + ":/" + altAl
+				vepInfo[mykey]={}      
+				#print ('#################' ) 
+				#print ( mykey )
+				#~~ check if the most severe consequence is in the line else retrun an empty vepInfo[mykey] 
+				if "most_severe_consequence" in info:
+					vepInfo[mykey]["most_severe_consequence"]=info["most_severe_consequence"]
+					most=vepInfo[mykey]["most_severe_consequence"]
+					#~~  check if the most sequence is in a transcript
+					if 'transcript_consequences' in info:
+						ReportedGeneId=[] ; ReportedGeneSymbol=[]
+						for tc in info['transcript_consequences']:
+							if most in  tc['consequence_terms'] :
+								csqAllele=tc['variant_allele']
+								if csqAllele ==altAl : 
+									#print (most, csqAllele, tc["transcript_id"], tc['gene_id'])
+									vepInfo[mykey]['csqAllele']=csqAllele
+									if not tc['gene_id'] in ReportedGeneId: ReportedGeneId.append(tc['gene_id'])
+									if not tc['gene_symbol'] in ReportedGeneSymbol:  ReportedGeneSymbol.append(tc['gene_symbol']) 
+						vepInfo[mykey]['gene_id']=ReportedGeneId
+						vepInfo[mykey]['gene_symbol']=ReportedGeneSymbol
+
+                                        #~~ check if the most severe consequence is in a regulatory feature  
+					elif 'regulatory_feature_consequences' in info: 
+						for rf  in info['regulatory_feature_consequences']:
+							if most in  rf['consequence_terms']:
+								csqAllele=rf['variant_allele']
+								if csqAllele ==altAl :
+									vepInfo[mykey]['csqAllele']=csqAllele
+						vepInfo[mykey]['gene_id']=[]
+						vepInfo[mykey]['gene_symbol']=[]
+
+                                        #~~ check if the most severe consequence is in an intergenic region 
+					elif 'intergenic_consequences' in info: 
+						for ic in info['intergenic_consequences']: 
+							if most in  ic['consequence_terms']: 
+								csqAllele=ic['variant_allele']
+								vepInfo[mykey]['csqAllele']=csqAllele	 
+						vepInfo[mykey]['gene_id']=[]
+						vepInfo[mykey]['gene_symbol']=[]
+
+				#print(vepInfo[mykey]) 
+				#~~ retrive info on rsID, starting position, frequencies for the csqAllele found in the previous code  
+				if "colocated_variants" in info:
+					id_search = info["colocated_variants"][0]
+					if "id" in id_search:
+						vepInfo[mykey]["id"] = id_search["id"]
+					#~~ adding info for starting position, this is needed for merging with CADD score.
+					if "start" in id_search: 
+						vepInfo[mykey]["starting_position"] = id_search["start"]   
+					#~~ check if variant is rare according to threshold given as argument and report True or False in vepInfo[mykey]
+					if 'frequencies' in id_search:
+						frqInfoDic=id_search["frequencies" ]
+						for i in frqInfoDic: 
+							if 'csqAllele' in vepInfo[mykey]: 
+								if vepInfo[mykey]['csqAllele'] in frqInfoDic: 
+									to_parse = list(frqInfoDic[vepInfo[mykey]['csqAllele']].values())
+									#print(to_parse)
+									vepInfo[mykey]['rare']= checkFreq (to_parse, thresholdRareAllele )
+                                                        	#else: vepInfo[mykey]['rare']='na'
+		
+
+	#print (json.dumps(info, indent=4 ) )  
+	#print (json.dumps(vepInfo, indent=4) ) 
+	return vepInfo
+
+
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def VepSOTermInfo (vepinfofile): 
     """read external file with info on VEP consequences  """
     lSOTerm=[]  ### list of SOTerm 
