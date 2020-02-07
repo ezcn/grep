05a29372f413156ced35234abcec0b819426a5f3
adrianodemarino <adriano.demarino@gmail.com>
2020-02-07 17:44:56 +0100

    speed up processing indexing

--- filtering/index_label_file_SNV_CADD.py
+++ filtering/index_label_file_SNV_CADD.py
@@ -1,20 +1,32 @@
+#rename file based on first and last row
+#python code
 
-
-
-for ogni_elemento in directory:
-	df = pd.read_csv(ogni_elemento,sep="\t",nrows=1)
-
-
-count= 
-df=pd.read_csv("SNV_key_ch4207.tsv", skiprows=range(2,count-1), header=0,sep="\t")
-
-import glob
-filenames = (glob.glob("SNV_key_ch*.tsv"))[0:10]
+import glob,os,sys
+#create a list of files
+print(">>> create a list of files")
+filenames = (glob.glob("SNV_key_ch*.tsv"))
+#count the lines in each files
+print(">>> count the lines in each files")
 len_of_dfs = [len(open(filename).readlines()) for filename in filenames]
+#read only first and last row in each file
+print(">>> read only first and last row in each file")
 list_of_dfs = [pd.read_csv(filename,skiprows=range(2,len_of_dfs[n]-1), header=0,sep="\t") for n,filename in enumerate(filenames)]
+#change name
+print(">>> changing name: START")
 for dataframe, filename in zip(list_of_dfs, filenames):
+	dataframe.loc[:,'filename_old'] = filename
 	dataframe.loc[:,"position"] = (dataframe.key.str.split(":",expand=True))[1]
 	dataframe.loc[:,"chr"] = (dataframe.key.str.split(":",expand=True))[0]
 	if dataframe.chr.nunique() == 1:
-		dataframe.loc[:,'filename'] = "SNV_key_chr{chr}_{start}-{end}.tsv".format(chr=dataframe.chr[0],start=dataframe.position[0],end=dataframe.position[1])
+		old = dataframe.filename_old[0]
+		new = dataframe.filename_new[0]
+		dataframe.loc[:,'filename_new'] = "SNV_key_chr{chr}_{start}-{end}.tsv".format(chr=dataframe.chr[0],start=dataframe.position[0],end=dataframe.position[1])
+		print(">> changing name file: {old} --->> {new}".format(old=old,new=new))
+		os.rename(old,new)
+	else:
+		print("ERROR: dataframe not contain unique chr in...QUIT!")
+		print(dataframe)
+		break
+print(">>> changing name: END")
 
+print("QUIT")
