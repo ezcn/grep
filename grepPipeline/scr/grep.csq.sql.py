0c0abda5d9354ffd824828c0d292986505da7762
SissiB <buonaiutosilvia@gmail.com>
2020-08-31 09:54:03 +0200

    modified grep.csq.sql.py

--- grepPipeline/scr/grep.csq.sql.py
+++ grepPipeline/scr/grep.csq.sql.py
@@ -20,6 +20,11 @@ def main():
 
 	conn = sqlite3.connect(args.db)  ### create new sql db
 	c = conn.cursor()
+	c.execute("DROP TABLE IF EXISTS myTable;")
+	c.execute("DROP TABLE IF EXISTS pLItable;")
+	c.execute("DROP TABLE IF EXISTS geneList;")
+	c.execute("DROP TABLE IF EXISTS fatmTab;")
+	c.execute("DROP TABLE IF EXISTS fatmNCtab;"
 	###### open vep table with pandas and create table inside grep.db 
 	df=pd.read_table(args.i, sep="\t", index_col= "Uploaded_variation")  ####apro file con pandas na_values="-"
 	df.columns = df.columns.str.strip()
